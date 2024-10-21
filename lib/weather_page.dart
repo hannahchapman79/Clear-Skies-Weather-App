ddbf3248f3e556544f1ab7576f8809bc92da0687
hannahchapman79 <hannahchapman1999@aol.com>
2024-10-21 14:38:47 +0800

    Adds coloured  widget to background and renders colour depending on weather icon

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -1,9 +1,10 @@
+import 'package:clearskies/weather_overview_card.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/intl.dart';
-import 'package:clearskies/weather_overview_card.dart';
+
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -12,7 +13,7 @@ class WeatherPage extends StatefulWidget {
   WeatherPageState createState() => WeatherPageState();
 }
 
-class WeatherPageState extends State<WeatherPage> {
+class WeatherPageState extends State<WeatherPage> { 
   Map<String, dynamic>? weatherData;
   bool isLoading = true;
 
@@ -53,7 +54,7 @@ class WeatherPageState extends State<WeatherPage> {
     DateTime localTime = utcTime.add(timezoneOffset);
 
     String formattedDate = DateFormat('EEE d').format(localTime);
-    String daySuffix = getDayOfMonthSuffix(localTime.day);
+    String daySuffix = getDayOfMonthSuffix(localTime.day); 
     String formattedMonth = DateFormat('MMMM').format(localTime);
 
     String formattedTime = DateFormat('HH:mm').format(localTime);
@@ -85,7 +86,6 @@ class WeatherPageState extends State<WeatherPage> {
 
   @override
   Widget build(BuildContext context) {
-    Map<String, String> dateTime = convertDate(weatherData!);
     if (isLoading) {
       return const Scaffold(
         body: Center(child: CircularProgressIndicator.adaptive()),
@@ -98,29 +98,67 @@ class WeatherPageState extends State<WeatherPage> {
       );
     }
 
+    Map<String, String> dateTime = convertDate(weatherData!);
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Clear Skies Weather App'),
       ),
-      body: Center(
-        child: Expanded(
-          child: WeatherOverviewItem(
-            date: dateTime["date"]!,
-            time: dateTime["time"]!,
-            location: weatherData!["name"],
-            temperature: weatherData!["main"]["temp"].toInt(),
-            apiIconCode: weatherData!["weather"][0]["icon"],
-            description: weatherData!["weather"][0]["description"],
-            feelsLike: weatherData!["main"]["feels_like"].toInt(),
-            onCityChanged: (newCity) {
-              setState(() {
-                isLoading = true;
-              });
-              fetchWeather(newCity);
-            },
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height *
+                  0.4, 
+              decoration: BoxDecoration(
+                color: () {
+                  String icon = weatherData!["weather"][0]["icon"];
+                  if (icon.endsWith("n")) {
+                    return const Color.fromRGBO(
+                        128, 49, 255, 1); // Purple for night
+                  } else if (icon == "01d") {
+                    return const Color.fromRGBO(
+                        255, 174, 0, 1); // Yellow for clear day
+                  } else if (icon == "02d" || icon == "04d" || icon == "03d") {
+                    return const Color.fromRGBO(
+                        0, 122, 255, 1); // Blue for partly cloudy
+                  } else {
+                    return const Color.fromARGB(255, 117, 108, 179); // Default purple/grey
+                  }
+                }(),
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+            ),
           ),
-        ),
+          Center(
+            child: 
+              Expanded(
+                child: WeatherOverviewItem(
+                  date: dateTime["date"]!,
+                  time: dateTime["time"]!,
+                  location: weatherData!["name"],
+                  temperature: weatherData!["main"]["temp"].toInt(),
+                  apiIconCode: weatherData!["weather"][0]["icon"],
+                  description: weatherData!["weather"][0]["description"],
+                  feelsLike: weatherData!["main"]["feels_like"].toInt(),
+                  onCityChanged: (newCity) {
+                    setState(() {
+                      isLoading = true;
+                    });
+                    fetchWeather(newCity);
+                  },
+                ),
+              ),
+          ),
+        ],
       ),
     );
   }
