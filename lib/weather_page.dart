a87235985c98d00fbb8eaaf7f30484e4e0f5283c
hannahchapman79 <hannahchapman1999@aol.com>
2024-10-21 10:59:10 +0800

    adds a date converting function to the project to enable readable date and time in the UI

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:intl/intl.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -32,11 +33,51 @@ class WeatherPageState extends State<WeatherPage> {
       if (response.statusCode == 200) {
         setState(() {
           weatherData = jsonDecode(response.body);
-          isLoading = false;
         });
       }
     } catch (e) {
       throw Exception('Error fetching weather: $e');
+    }
+  }
+
+    Map<String, String> convertDate(Map<String, dynamic> weatherData) {
+    int timezoneShiftInSeconds = weatherData["timezone"];
+    int timestamp = weatherData["dt"];
+
+    DateTime utcTime =
+        DateTime.fromMillisecondsSinceEpoch(timestamp * 1000, isUtc: true);
+
+    Duration timezoneOffset = Duration(seconds: timezoneShiftInSeconds);
+    DateTime localTime = utcTime.add(timezoneOffset);
+
+    String formattedDate = DateFormat('EEE d').format(localTime);
+    String daySuffix = getDayOfMonthSuffix(localTime.day); 
+    String formattedMonth = DateFormat('MMMM').format(localTime);
+
+    String formattedTime = DateFormat('HH:mm').format(localTime);
+
+    return {
+      "date": '$formattedDate$daySuffix $formattedMonth',
+      "time": formattedTime,
+    };
+  }
+
+  String getDayOfMonthSuffix(int day) {
+    if (!(day >= 1 && day <= 31)) {
+      throw Exception('Invalid day of month');
+    }
+    if (day >= 11 && day <= 13) {
+      return 'th';
+    }
+    switch (day % 10) {
+      case 1:
+        return 'st';
+      case 2:
+        return 'nd';
+      case 3:
+        return 'rd';
+      default:
+        return 'th';
     }
   }
 }
